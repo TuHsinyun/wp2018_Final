15aef7ea37c9e65318e19ffc88f1a331dcaff9bd
leokuo0724 <p900372012@gmail.com>
2018-10-22 23:46:23 +0800

    loading revise

--- about us/html/main.js
+++ about us/html/main.js
@@ -1,4 +1,5 @@
 $(window).load(function(){
+  $('html, body').animate({scrollTop:0},200);
   $(".loading").fadeOut(1000,function(){
     $("#project-wrapper").animate({width:'100vw',opacity :'1'},600,function(){
         setTimeout($('body').css({'overflow':'auto'}),1000);
