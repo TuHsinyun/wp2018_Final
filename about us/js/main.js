0ff6aa0d8545f7b01fa5c26830f842b9f5837639
leokuo0724 <p900372012@gmail.com>
2018-10-24 16:51:31 +0800

    test

---
+++ about us/js/main.js
@@ -0,0 +1,34 @@
+$(window).load(function(){
+  $('html,body').animate({scrillTop:0},200);
+    $(".loading").fadeOut(1000,function(){
+      $("#project-wrapper").animate({width:'100vw',opacity :'1'},600,function(){
+        setTimeout($('body').css({'overflow':'auto'}),1000);
+        $("#members-wrapper").css("display","block");
+        $("::-webkit-scrollbar").css("display","none");
+    });
+  });
+});
+
+$(document).ready(function(){
+var arrow = $("#arrow-down img , ul li:eq(0)"),
+    scroll_bottom = $("body").height()+$(window).height(),
+    button_project = $("ul li:eq(1)"),
+    $body = (window.opera) ? (document.compatMode =="CSS1Compat" ? $('html') : $('body')):$('html,body');
+
+  arrow.click(function(){
+    $body.stop().animate({
+      scrollTop:scroll_bottom},850);
+  });
+
+  button_project.click(function(){
+    $body.stop().animate({
+      scrollTop:0},650);
+  });
+
+
+});
+
+
+
+
+
