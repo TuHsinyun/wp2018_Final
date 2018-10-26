cde84f599ad369723a1b5bf8960593f989d6131b
wtyo1768 <wtyo1768@gmail.com>
2018-10-26 17:34:04 +0800

    Little update

--- about us/js/main.js
+++ about us/js/main.js
@@ -1,19 +1,18 @@
 $(window).load(function(){
   $('html,body').animate({scrillTop:0},200);
-    $(".loading").fadeOut(1000,function(){
-      $("#project-wrapper").animate({width:'100vw',opacity :'1'},600,function(){
-        setTimeout($('body').css({'overflow':'auto'}),1000);
-        $("#members-wrapper").css("display","block");
-        $("::-webkit-scrollbar").css("display","none");
+  $(".loading").fadeOut(1000,function(){
+    $("#project-wrapper").animate({width:'100vw',opacity :'1'},600,function(){
+      setTimeout($('body').css({'overflow':'auto'}),1000);
+      $("#members-wrapper").css("display","block");
     });
   });
 });
 
 $(document).ready(function(){
-var arrow = $("#arrow-down img , ul li:eq(0)"),
-    scroll_bottom = $("body").height()+$(window).height(),
-    button_project = $("ul li:eq(1)"),
-    $body = (window.opera) ? (document.compatMode =="CSS1Compat" ? $('html') : $('body')):$('html,body');
+  var arrow = $("#arrow-down img , #nav-items li:eq(0)"),
+  scroll_bottom = $("body").height()+$(window).height(),
+  button_project = $("#nav-items li:eq(1)"),
+  $body = (window.opera) ? (document.compatMode =="CSS1Compat" ? $('html') : $('body')):$('html,body');
 
   arrow.click(function(){
     $body.stop().animate({
@@ -43,8 +42,26 @@ var arrow = $("#arrow-down img , ul li:eq(0)"),
     // setTimeout(()=>{$(element).css({'display':'none'});},300);
   })
 
-});
+  $(".member").click(function(){
+    var member = this.id;
 
+    switch(member){
+      case 'member1':
+
+        break;
+      case 'member1':
+        break;
+      case 'member2':
+        break;
+      case 'member3':
+        break;
+      case 'member4':
+        break;
+      case 'member5':
+        break;
+    }
+  })
+});
 
 
 
